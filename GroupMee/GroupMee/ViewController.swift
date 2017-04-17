23f4b88adb4ca6f884d7903e915a7d800cf5da81
DoidoYo <gabrielfernandes@Gabriels-MacBook-Pro.local>
2017-04-17 11:20:38 -0400

    Created project

---
+++ GroupMee/GroupMee/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GroupMee
+//
+//  Created by Gabriel Fernandes on 4/17/17.
+//  Copyright © 2017 Gabriel Fernandes. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
